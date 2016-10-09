9a169a1d619c09dfedca52d56e15c72c42026a80
Tiger Huang <tigerrun1998@gmail.com>
2016-10-09 13:30:37 -0400

    Added the ability to run init and step code.

--- simulation.js
+++ simulation.js
@@ -6,6 +6,14 @@ var States = Object.freeze({stopped: 0,
 var state = States.stopped;
 var timerID;
 
+var ins;
+var fns;
+var outs;
+var tmps;
+var tindex;
+var uis;
+var uindex;
+
 //Writing button labels
 function apply_labels() {
 	var start_button = document.getElementById('start_button');
@@ -79,8 +87,7 @@ function setState(new_state) {
 			break;
 		case States.running:
 			state = States.running;
-			init();
-			resume();
+			init() && resume();
 			break;
 		}
 		break;
@@ -112,28 +119,82 @@ function setState(new_state) {
 }
 
 function init() {
-	//Actual init stuff now
-	console.log("INIT");
-
-}
-
-function resume() {
-	var timestep = document.getElementById('codestep_textfield').value;
+	var timestep = document.getElementById('timestep_textfield').value;
 
 	//Bail early if we have any errors
 	if(isNaN(timestep) || !timestep) {
-		alert("Code Run Timestep is not a number");
-		setState(States.paused);
-		return;
+		alert("Simulation Timestep is not a number");
+		setState(States.stopped);
+		return false;
+	}
+	if(+timestep == 0) {
+		alert("Simulation Timestep cannot be zero");
+		setState(States.stopped);
+		return false;
 	}
 
-	console.log("RESUME");
-	timerID = window.setInterval(step, +timestep);
+	//Actual init stuff now
+	console.log("INIT");
+
+	internal_init(+timestep / 1000);
+
+	return true;
+}
+
+function internal_init(timestep) {
+	ins = {};
+	outs = {};
+	tmps = [];
+	uis = [];
+
+	ins.tick = 0;
+	ins.t = 0;
+	ins.dt = timestep;
+	ins.init = true;
+	tindex = 0;
+	unidex = 0;
+
+	//Stuff in here should only write back to ins
+	var init_code = document.getElementById('init_textarea').value;
+	eval(init_code);
+	tmps = []; //Just in case
+
+	internal_step(timestep, true);
+}
+
+function internal_step(timestep, initialize) {
+	ins.dt = timestep;
+	ins.init = initialize;
+	tindex = 0;
+
+	var step_code = document.getElementById('step_textarea').value;
+	eval(step_code);
+	outs.tick = ins.tick + 1;
+	outs.t = outs.tick * timestep;
+	for(var k in outs) ins[k] = outs[k];
+}
+
+function resume() {
+	var codestep = document.getElementById('codestep_textfield').value;
+
+	//Bail early if we have any errors
+	if(isNaN(codestep) || !codestep) {
+		alert("Code Run Timestep is not a number");
+		setState(States.paused);
+		return false;
+	}
+
+	//console.log("RESUME");
+	timerID = window.setInterval(step, +codestep);
+
+	return true;
 }
 
 function pause() {
-	console.log("PAUSE");
+	//console.log("PAUSE");
 	window.clearInterval(timerID);
+
+	return true;
 }
 
 function step() {
@@ -143,14 +204,42 @@ function step() {
 	if(isNaN(timestep) || !timestep) {
 		alert("Simulation Timestep is not a number");
 		setState(States.paused);
-		return;
+		return false;
 	}
 	if(+timestep == 0) {
 		alert("Simulation Timestep cannot be zero");
 		setState(States.paused);
-		return;
+		return false;
 	}
 
 	//Do actual stepping stuff now
 	console.log("STEP");
+
+	internal_step(+timestep / 1000, false);
+
+	return true;
 }
+
+//Functions
+fns = {};
+function addFunction(name, func) {
+	fns[name] = function() {
+		if(ins.init) {
+			tmps[tindex] = {};
+		}
+		return func(arguments, tmps[tindex++]);
+	}
+}
+fns.log = function(text) {
+	document.getElementById('out_textarea').value += text + '\n';
+}
+addFunction("integral", function(args, tmp) {
+	if(ins.init) {
+		tmp.acc = 0;
+		return 0;
+	}
+	else {
+		tmp.acc += args[0] * ins.dt;
+		return tmp.acc;
+	}
+});
